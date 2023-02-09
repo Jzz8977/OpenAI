73e49ef70ff6dad4e616ec1ab3f5d7975cf8c7c0
Jzz8977 <jzz8977@163.com>
2023-02-10 03:51:49 +0800

    wechat ai template

---
+++ wechat-ai/custom-tab-bar/data.js
@@ -0,0 +1,16 @@
+export default [{
+    icon: 'home',
+    text: '首页',
+    url: 'pages/home/home',
+  },
+  {
+    icon: 'chat',
+    text: '购物车',
+    url: 'pages/cart/index',
+  },
+  {
+    icon: 'person',
+    text: '个人中心',
+    url: 'pages/usercenter/index',
+  },
+];

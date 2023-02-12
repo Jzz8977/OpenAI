5c1c3d890523d332325faee93f2903a3ea8ab769
Jzz8977 <jzz8977@163.com>
2023-02-12 23:47:44 +0800

    add server api path /bb

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,30 @@ app.post("/", async (req, res) => {
   }
 });
 
+app.post("/bb", async (req, res) => {
+  try {
+    const question = req.query.question;
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${question}`,
+      temperature: 0, // Higher values means the model will take more risks.
+      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    });
+    // console.log(response);
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    // console.error(error);
+    res.status(500).send(error || "Something went wrong");
+  }
+});
+
+
 app.listen(8000, () => {
   console.log("App is running");
 });
